d5f022ef65e2e9282da8aa3fe3055e03cf9e469f
coderxuz <coderxuz2009@gmail.com>
2026-06-28 12:30:48 +0500

    standalone remove

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-  output: 'standalone'
-=======
   allowedDevOrigins: [
     "10.119.107.22",
   ],
->>>>>>> d22872538c5a4119469485455613a25c086899d0
 };
 
 export default nextConfig;
